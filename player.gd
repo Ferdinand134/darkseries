6b166ebe53d3ed9e51232418ac33e747e6e61286
fkorantengbarnes <fkorantengbarnes@ucvts.org>
2024-05-08 08:07:14 -0400

    animation

---
+++ player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+const SPEED = 2000
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$AnimatedSprite2D.play("default")
+	
+func _physics_process(delta):
+	var direction = Vector2(0,0)
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+	velocity = direction.normalized() * SPEED
+	move_and_slide()
+
+
+
